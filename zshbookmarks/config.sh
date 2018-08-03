a3db3552615c4e58ce41977cb7b572235c67f0a7
JasonLong24 <jasonlongball@gmail.com>
2018-08-03 19:44:48 -0400

    big to zshbookmark

---
+++ zshbookmarks/config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Config for different file types
+
+BM_OPEN_PDF="zathura"
+BM_OPEN_AUDIO="mpv"
+BM_OPEN_VIDEO="mpv"
+BM_OPEN_HTML="firefox"
+BM_OPEN_CSV="scim"
+BM_OPEN_TEXT="vim"
--- zshbookmarks/zshbookmark.sh
+++ zshbookmarks/zshbookmark.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+BM_CONFIG="$HOME/git/shell-plugins/zshbookmarks/config.sh"
 BM_PATH="$HOME/.dirbookmarks"
+EXEC_ADD=false
+EXEC_REMOVE=false
+EXEC_CONFIG=false
+EXEC_LIST=false
+source $BM_CONFIG 
 
 function config() {
   if [[ -d ${CONFIG} ]] || [[ $(echo ${CONFIG} | grep -o .dirbookmarks) = "" ]]; then
     echo Please Specify a .dirbookmarks file.
-    exit 1
+    return 1
   else
     BM_PATH="${CONFIG}"
   fi
@@ -15,27 +21,27 @@ function add() {
   if [[ -d ${ADD_DIR} ]]; then
     if [[ $(cat $BM_PATH | grep -w -o "${ADD_ALIAS}") = ${ADD_ALIAS} ]]; then
       echo ${ADD_ALIAS} is already bound.
-      exit 1
+      return 1
     else
       echo ${ADD_ALIAS} ${ADD_DIR} >> $BM_PATH
     fi
   else
     echo ${ADD_DIR} is not a directory.
-    exit 1
+    return 1
   fi
 }
 
 function remove() {
   if [[ $(cat $BM_PATH | grep -w -o "${REMOVE}") = "" ]]; then
     echo ${REMOVE} is not bound.
-    exit 1
+    return 1
   else
     removeline=$(cat $BM_PATH | grep -w -n "${REMOVE}" | cut -d : -f 1)
-    read -p "This will remove ($(cat $BM_PATH | awk NR==$removeline | sed -e 's/ / -> /g')) from your alias list. Are you sure? (y/n) " -n 1;
+    read "?This will remove ($(cat $BM_PATH | awk NR==$removeline | sed -e 's/ / -> /g')) from your alias list. Are you sure? (y/n) "
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       sed -i "$removeline d" $BM_PATH
     else
-      exit 1
+      return 1
     fi
   fi
 }
@@ -43,7 +49,41 @@ function remove() {
 function list() {
   echo -e Your current aliases:"\n"
   cat $BM_PATH | sed -e 's/ / -> /g'
-  exit 0
+  return 0
+}
+
+function bookmark() {
+  if [[ $EXEC_ADD = false ]] && [[ $EXEC_REMOVE = false ]] && [[ $EXEC_LIST = false ]]; then
+    BOOKMARK_NAME=$(echo ${BOOKMARK} | cut -d : -f1)
+    if [[ $(cat $BM_PATH | grep -w -o "${BOOKMARK_NAME}") = "" ]]; then
+      echo ${BOOKMARK_NAME} is not bound. Try using --add.
+      return 1
+    else
+      BOOKMARK_EXTRA=$(echo ${BOOKMARK} | awk -F":" '{print $2}')
+      BOOKMARK_LINE=$(cat $BM_PATH | awk NR==$(cat $BM_PATH | grep -w -n "${BOOKMARK_NAME}" | awk -F":" '{print $1}')'{print $2}')
+      BOOKMARK_FULL=$(echo $BOOKMARK_LINE"/"$BOOKMARK_EXTRA)
+      if [[ -f $BOOKMARK_FULL ]]; then
+        BOOKMARK_FILE=$(echo $BOOKMARK_EXTRA | cut -d . -f2)
+        bookmarkFile
+      elif [[ -d $BOOKMARK_FULL ]]; then
+        cd $BOOKMARK_FULL 
+      else
+        echo No such file or directory. $BOOKMARK_FULL 
+        return 1
+      fi
+    fi
+  fi
+}
+
+function bookmarkFile() {
+  case $BOOKMARK_FILE in
+    pdf|djvu) $BM_OPEN_PDF $BOOKMARK_FULL ;;
+    html) $BM_OPEN_HTML $BOOKMARK_FULL ;;
+    mp3|ogg|wav) $BM_OPEN_AUDIO $BOOKMARK_FULL ;;
+    sc|csv|sxc|xlsx) $BM_OPEN_CSV $BOOKMARK_FULL ;;
+    avi|mp4) $BM_OPEN_VIDEO $BOOKMARK_FULL ;;
+    *) $BM_OPEN_TEXT $BOOKMARK_FULL ;;
+  esac
 }
 
 function help() {
@@ -56,9 +96,9 @@ do
   key="$1"
   case $key in
     -c|--config)
-      if [[ $2 = "" ]] || [[ $3 =~ ^- ]]; then
+      if [[ $2 = "" ]] || [[ $2 =~ ^- ]]; then
         echo Config takes one argument. See --help
-        exit 1
+        return 1
       else
         CONFIG="$2"
         shift
@@ -68,7 +108,7 @@ do
     -a|--add)
       if [[ $2 = "" ]] || [[ $2 =~ ^- ]] || [[ $3 = "" ]] || [[ $3 =~ ^- ]]; then
         echo Add takes two arguments. See --help
-        exit 1
+        return 1
       else
         ADD_DIR="$2"
         ADD_ALIAS="$3"
@@ -79,7 +119,7 @@ do
     -r|--remove)
       if [[ $2 = "" ]] || [[ $2 =~ ^- ]]; then
         echo Remove takes one arguments. See --help
-        exit 1
+        return 1
       else
         REMOVE="$2"
         shift
@@ -88,24 +128,21 @@ do
       fi ;;
     -l|--list)
       EXEC_LIST=true 
-      shift
       shift ;;
     -h|--help)
       help
-      exit 0 ;;
+      return 0 ;;
     *)    
       POSITIONAL+=("$1") 
+      BOOKMARK="$1"
+      bookmark
       shift ;;
   esac
 done
 set -- "${POSITIONAL[@]}" 
 if [[ ! -e $BM_PATH ]]; then touch .dirbookmarks; fi
 if [[ $EXEC_CONFIG = true ]]; then config; fi
-if [[ $EXEC_ADD = true ]] && [[ $EXEC_REMOVE = true ]]; then echo You cannot remove and add in one statement. See --help; exit 1; fi
+if [[ $EXEC_ADD = true ]] && [[ $EXEC_REMOVE = true ]]; then echo You cannot remove and add in one statement. See --help; return 1; fi
 if [[ $EXEC_ADD = true ]]; then add; fi
 if [[ $EXEC_REMOVE = true ]]; then remove; fi
 if [[ $EXEC_LIST = true ]]; then list; fi
-
-if [[ $# = 0 ]]; then
-  echo command goes here
-fi
