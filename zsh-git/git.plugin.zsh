5abb4f37b368da302ac3e9d140923d7259dcdfb1
JasonLong24 <jasonlongball@gmail.com>
2018-10-27 20:38:50 -0400

    to null

--- zsh-git/git.plugin.zsh
+++ zsh-git/git.plugin.zsh
@@ -87,7 +87,7 @@ function git_staged() {
 }
 
 function git_full_prompt() {
-  git_toplevel=$(git rev-parse --show-toplevel)
+  git_toplevel=$(git rev-parse --show-toplevel 2>/dev/null)
   if is_repo; then
     if is_clean; then; SEPERATOR=""; else; SEPERATOR="|"; fi
     echo "($(git_branch)$SEPERATOR$(git_status others)$(git_status modified)$(git_status deleted)$(git_status unmerged)$(git_staged))"
