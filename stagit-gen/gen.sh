6d41bc4162498e4e67d505ca3347082ae34b7178
JasonLong24 <jasonlongball@gmail.com>
2018-12-03 20:06:10 -0500

    some updates to stagit gen

--- stagit-gen/gen.sh
+++ stagit-gen/gen.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 REPO_PATH="repos"
-repos_length=$(cat $REPO_PATH | wc -l)
-EXEC_REPOS=false
+repos_length=$(cat $REPO_PATH 2>/dev/null | wc -l)
+
+EXEC_REPOS_PATH=false
+EXEC_REPO=false
+EXEC_INDEX=false
 EXEC_STYLE=false
 EXEC_GENALL=false
 
@@ -11,6 +14,7 @@ function checkRepos() {
     echo Please Specify a repos file.
     exit 1
   else
+    echo Found ${REPO_SET}
     REPO_PATH="${REPO_SET}"
   fi
 }
@@ -18,7 +22,7 @@ function checkRepos() {
 function genRepos() {
   for (( i=1;i<=$repos_length;i++ )); do
     repos=$(cat $REPO_PATH | awk NR==$i)
-    echo  "-> Found" $(basename $repos) 
+    echo  "-> Found" $(basename $repos)
     mkdir $(basename $repos) && cd $(basename $repos)
     stagit -c .cache $repos
     cp ../style.css .
@@ -27,6 +31,7 @@ function genRepos() {
 }
 
 function genIndex() {
+  echo Generating index
   stagit-index $(cat $REPO_PATH) > index.html
 }
 
@@ -40,14 +45,18 @@ function genStyle() {
 
 function genClear() {
   echo Clearing directory
-  rm *.html 2>/dev/null
+  rm *.xml *.html 2>/dev/null
   rm -rf ./*/ 2>/dev/null
 }
 
 function genAll() {
-  genClear
-  genIndex
-  genRepos
+  if [[ $EXEC_REPO = true ]] || [[ $EXEC_INDEX = true ]]; then
+    echo "You cannot run -a with -r or -i. See --help" && exit 1
+  else
+    genClear
+    genIndex
+    genRepos
+  fi
 }
 
 function help() {
@@ -59,16 +68,16 @@ while [[ $# -gt 0 ]]
 do
   key="$1"
   case $key in
-    -r|--repos)
+    -rp|--repos-path)
       if [[ $2 = "" ]] || [[ $2 =~ ^- ]]; then
         echo repos takes one argument. See --help
         exit 1
       else
         REPO_SET="$2"
       fi
-      shift 
       shift
-      EXEC_REPOS=true
+      shift
+      EXEC_REPOS_PATH=true
       ;;
     -s|--style)
       if [[ $2 = "" ]] || [[ $2 =~ ^- ]]; then
@@ -77,27 +86,35 @@ do
       else
         STYLE_PATH="$2"
       fi
-      shift 
+      shift
       shift
       EXEC_STYLE=true
       ;;
+    -r|--repo)
+      EXEC_REPO=true
+      shift ;;
+    -i|--index)
+      EXEC_INDEX=true
+      shift ;;
     -a|--all)
       EXEC_GENALL=true
       shift ;;
     -c|--clear)
       genClear
       exit 0 ;;
-    -h|--help) 
+    -h|--help)
       help
       exit 0 ;;
-    *)    
-      POSITIONAL+=("$1") 
-      shift 
+    *)
+      POSITIONAL+=("$1")
+      shift
       ;;
   esac
 done
-set -- "${POSITIONAL[@]}" 
+set -- "${POSITIONAL[@]}"
 
-if [[ $EXEC_REPOS = true ]]; then checkRepos; fi
+if [[ $EXEC_REPOS_PATH = true ]]; then checkRepos; fi
 if [[ $EXEC_STYLE = true ]]; then genStyle; fi
 if [[ $EXEC_GENALL = true ]]; then genAll; fi
+if [[ $EXEC_REPO = true ]]; then genClear && genRepos; fi
+if [[ $EXEC_INDEX = true ]]; then genIndex; fi
