c415ca4f5d1e64597f2ef865700ff5810ee50d31
JasonLong24 <jasonlongball@gmail.com>
2019-02-23 13:57:40 -0500

    refactor git dot so easily add new modes

--- zsh-git/git.plugin.zsh
+++ zsh-git/git.plugin.zsh
@@ -8,6 +8,7 @@ purple="%{%F{purple}%}"
 cyan="%{%F{cyan}%}"
 white="%{%F{white}%}"
 reset="%{%F{white}%}"
+ZSH_GIT_PROMPT_TYPE="dot"
 
 function dot_git() {
   dot_git="$(git rev-parse --git-dir 2>/dev/null)"
@@ -44,25 +45,15 @@ function git_branch() {
   fi
 
   if [[ $(git rev-parse --show-superproject-working-tree) = "" ]]; then
-    get_clean
-    echo ${RET_COL}$(git symbolic-ref --short HEAD 2>/dev/null)$(get_build)${reset}${CHECK}
+    if [[ $ZSH_GIT_PROMPT_TYPE = "dot" ]]; then echo ${RET_COL}$(git symbolic-ref --short HEAD 2>/dev/null)${reset}; else
+      get_clean
+      echo ${RET_COL}$(git symbolic-ref --short HEAD 2>/dev/null)$(get_build)${reset}${CHECK}
+    fi
   else
-    get_clean
-    echo ${RET_COL}$(git rev-parse @ | cut -c -7 2>/dev/null)$(get_build)${reset}${CHECK}
-  fi
-}
-
-function git_branch_dots() {
-  if is_clean; then
-    RET_COL=$green
-  else
-    RET_COL=$red
-  fi
-
-  if [[ $(git rev-parse --show-superproject-working-tree) = "" ]]; then
-    echo ${RET_COL}$(git symbolic-ref --short HEAD 2>/dev/null)${reset}
-  else
-    echo ${RET_COL}$(git rev-parse @ | cut -c -7 2>/dev/null)${reset}
+    if [[ $ZSH_GIT_PROMPT_TYPE = "dot" ]]; then echo ${RET_COL}$(git rev-parse @ | cut -c -7 2>/dev/null)${reset}; else
+      get_clean
+      echo ${RET_COL}$(git rev-parse @ | cut -c -7 2>/dev/null)$(get_build)${reset}${CHECK}
+    fi
   fi
 }
 
@@ -104,28 +95,18 @@ function get_dots() {
 function git_status() {
   info=$(git ls-files --$1 --exclude-standard $git_toplevel 2>/dev/null | wc -l)
   if [[ $info = 0 || -z $info ]]; then echo ''; else
-    echo $(get_symbol $1)$info
-  fi
-}
-
-function git_status_dots() {
-  info=$(git ls-files --$1 --exclude-standard $git_toplevel 2>/dev/null | wc -l)
-  if [[ $info = 0 || -z $info ]]; then echo ''; else
-    echo $(get_dots $1)
+    if [[ $ZSH_GIT_PROMPT_TYPE = "dot" ]]; then echo $(get_dots $1); else
+      echo $(get_symbol $1)$info
+    fi
   fi
 }
 
 function git_staged() {
   stage=$(git diff --name-only --cached | wc -l)
   if [[ $stage = 0 || -z $stage ]]; then echo ''; else
-    echo $yellow● ${reset}$stage
-  fi
-}
-
-function git_staged_dot() {
-  stage=$(git diff --name-only --cached | wc -l)
-  if [[ $stage = 0 || -z $stage ]]; then echo ''; else
-    echo $yellow🌑${reset}
+    if [[ $ZSH_GIT_PROMPT_TYPE = "dot" ]]; then echo $yellow🌑${reset}; else
+      echo $yellow🌑 ${reset}$stage
+    fi
   fi
 }
 
@@ -151,16 +132,7 @@ function build_type() {
 function git_full_prompt() {
   git_toplevel=$(git rev-parse --show-toplevel 2>/dev/null)
   if is_repo; then
-    echo "$(git_branch)$(git_status others)$(git_status modified)$(git_status deleted)$(git_status unmerged)$(git_staged)"
-  else
-    echo ""
-  fi
-}
-
-function git_full_dot_prompt() {
-  git_toplevel=$(git rev-parse --show-toplevel 2>/dev/null)
-  if is_repo; then
-    echo "[$(git_branch_dots)$(git_status_dots others)$(git_status_dots modified)$(git_status_dots deleted)$(git_status_dots unmerged)$(git_staged_dot)]"
+    echo "[$(git_branch)$(git_status others)$(git_status modified)$(git_status deleted)$(git_status unmerged)$(git_staged)]"
   else
     echo ""
   fi
